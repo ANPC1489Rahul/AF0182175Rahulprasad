a7cf1d188be0c8b07ac9d5faeb453830ed76338d
ANPC1489Rahul <105110428+ANPC1489Rahul@users.noreply.github.com>
2022-08-08 13:30:06 +0530

    Add files via upload

---
+++ function in java/Main.java
@@ -0,0 +1,25 @@
+public class Main{
+
+    public static double salaryCalculator(double hoursPerWeek, double amountPerHour, int vacationDays){
+        
+        if(hoursPerWeek < 0){
+            return -1;
+        }
+
+        if(amountPerHour < 0){
+            return -1;
+        }
+
+        double weeklypaycheck = hoursPerWeek * amountPerHour;
+        double unpaidTime = vacationDays * amountPerHour * 8;
+        return (weeklypaycheck * 52) - unpaidTime;
+    }
+    
+    public static void main(String[] args) {
+        int hoursPerWeek;
+        int amountPerHour;
+        int vacationDays;
+        double salary = salaryCalculator(hoursPerWeek: 40, amountPerHour: 15, vacationDays: 8);
+        System.out.println(salary);
+    }
+}
