68c59cb4ac6592e1a97d5da3788d31e3ba82a00c
ANPC1489Rahul <105110428+ANPC1489Rahul@users.noreply.github.com>
2022-09-18 13:42:25 +0530

    Add files via upload

---
+++ Mysql 1/query 1.1.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE schools
+
+use schools
+
+CREATE TABLE users(id int unsigned,name varchar(100),email varchar(150),password varchar(100),contact varchar(15),address text,dob date,gender enum('M','F','O'),status boolean)
+
